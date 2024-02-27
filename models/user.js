b7ed5796b4c3574d91a86837352c9a80439d0912
MarkRBishop <mrb.8@hotmail.com>
2024-02-27 16:59:02 -0600

    added a little security to users with bcrypt, also added a simple timestamp helper

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt')
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {checkPassword(loginPw) {
+  return bcrypt.compareSync(loginPw, this.password);
+}}
 
 User.init(
   {
@@ -15,10 +18,17 @@ User.init(
     username: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      }
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: [8],
+      },
     },
     created_at: {
       type: DataTypes.DATE,
@@ -26,9 +36,19 @@ User.init(
     },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
     sequelize,
     modelName: 'user',
-    timestamps: false,
+    timestamps: true,
     underscored: true,
   }
 );
--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 // server.js
+const path = require('path')
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars')
 const routes = require('./controllers');
+const helpers = require('./utils/helpers')
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers })
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Session middleware
 app.use(session({
-  secret: 'proces.env.SESSION_SECRET',
+  secret: proces.env.SESSION_SECRET,
+  cookie: {
+    maxAge:600000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 }));
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
 // Routes
 app.use(routes);
